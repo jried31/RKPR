7dc1c258a5f8387fe6596a7bc2a10e87442418e0
jried31 <jried31@192.168.1.17>
2013-10-22 07:52:12 -0700

    first commit

---
+++ RideKeeper/src/com/example/android/navigationdrawerexample/DBGlobals.java
@@ -0,0 +1,6 @@
+package com.example.android.navigationdrawerexample;
+
+ class DBGlobals {
+    public static final String DB_SCHEMA_NAME = "RIDEKEEPER";
+	public static final int MY_PROFILE=8, MY_VEHICLE=9, STOLEN_VEHICLES=2; 
+}
