39fa8987f9736283282cbf11a2ed9643cb7b08f7
Phil Crumm <pcrumm@p3net.net>
2013-11-23 10:31:16 -0800

    Code quality cleanup

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-//Check tutorials to learn how to start a simple node.js server
-//copy and paste the following into a new js file (eg: server.js)
 var restify = require('restify');
 var request = require('request');
 var Kaiseki = require('kaiseki');
@@ -16,27 +14,26 @@ var kaiseki = new Kaiseki(APP_ID, REST_API_KEY);
 
 function timestamp()
 {
-var d = new Date();
-var month = d.getMonth() + 1,
-date = d.getDate(),
-year = d.getFullYear(),
-hour = d.getHours()-2,
-min = d.getMinutes(),
-sec = d.getSeconds();
+	var d 		= new Date();
+	var month 	= d.getMonth() + 1,
+	var date 	= d.getDate(),
+	var year 	= d.getFullYear(),
+	var hour 	= d.getHours() -2 ,
+	var min 	= d.getMinutes(),
+	var sec 	= d.getSeconds();
 
-if(month < 10)
-month = '0' + month;
-if(date < 10)
-date = '0' + date;
+	if(month < 10)
+		month = '0' + month;
+	if(date < 10)
+		date = '0' + date;
+	if(hour < 10)
+		hour = '0' + hour;
+	if(min < 10)
+		min = '0' + min;
+	if(sec < 10)
+		sec = '0' + sec;
 
-if(hour < 10)
-hour = '0' + hour;
-if(min < 10)
-min = '0' + min;
-if(sec < 10)
-sec = '0' + sec;
-
-return month + '-' + date + '-' + year + " " + hour + ":" + min + ":" + sec;
+	return month + '-' + date + '-' + year + " " + hour + ":" + min + ":" + sec;
 }
 
 function updateParse(req,res,next){
@@ -59,19 +56,25 @@ function updateParse(req,res,next){
 }
 
 function forward(req,res,next){
-res.send(JSON.stringify(req.body));
-console.log('Got Data \n %s',req.body);
-var tmpObj = JSON.parse(JSON.stringify(req.body));
-tmpObj.time = timestamp() ;
-var id = tmpObj.id;
-delete tmpObj.id;
-request.post({
-headers: {'User-Agent' : 'curl/7.26.0','X-Parse-Application-Id' : 'OZzFan5hpI4LoIqfd8nAJZDFZ3ZLJ70ZvkYCNJ6f','X-Parse-REST-API-Key' : 'bPlqPguhK51mbRXaYcfnf73uTri07sk6uB64ZdPb','content-type' : 'application/json'},
-url: '107.22.188.163:8080/1/classes/Vehicle/bht5oha6ix',
-body: JSON.stringify(tmpObj)
-}, function(error, response, body){
-console.log(body);
-});
+	res.send(JSON.stringify(req.body));
+	console.log('Got Data \n %s',req.body);
+
+	var tmpObj = JSON.parse(JSON.stringify(req.body));
+	tmpObj.time = timestamp();
+
+	var id = tmpObj.id;
+	delete tmpObj.id;
+	request.post({
+		headers: {
+			'User-Agent' : 'curl/7.26.0','X-Parse-Application-Id' : APP_ID,
+			'X-Parse-REST-API-Key' : REST_API_KEY,
+			'content-type' : 'application/json'
+		},
+		url: '107.22.188.163:8080/1/classes/Vehicle/bht5oha6ix',
+		body: JSON.stringify(tmpObj)
+	}, function(error, response, body){
+		console.log(body);
+	});
 }
 var server = restify.createServer();
 server.use(restify.bodyParser({ mapParams: false }));
@@ -80,9 +83,8 @@ server.post('/update', updateParse);
 server.post('/forward', forward);
 
 server.listen(8080, function() {
-console.log('%s listening at %s', server.name, server.url);
+	console.log('%s listening at %s', server.name, server.url);
 });
-//END THE COPY/PASTE step
 /*
  * do "npm install restify" if it's missing that dependency
  * and "npm install" afterwards to install any other missing files
