7e6cd5e53b5255f0442686ec01703b7b27278f30
thenickot2 <ntsianos@ucla.edu>
2013-12-02 16:13:51 -0800

    Fixed a format bug. Alert Level is passed in as a string, not an int

--- server.js
+++ server.js
@@ -85,11 +85,11 @@ function updateVehicleStatus(req,resp,next){
 	};
   
 	kaiseki.updateObject('Vehicle', tmpObj.id,
-	                   {'alertLevel': AlertLevel.getKeyByValue(tmpObj.alertLevel), 
+	                   {'alertLevel': AlertLevel.getKeyByValue(parseInt(tmpObj.alertLevel)), 
 	                    'pos': position.location},
 	          function(err, res, body, success) {
 	          	if (success) {
-	          		console.log('Marked ' + tmpObj.id + ' as ' + AlertLevel.getKeyByValue(tmpObj.alertLevel));
+	          		console.log('Marked ' + tmpObj.id + ' as ' + AlertLevel.getKeyByValue(parseInt(tmpObj.alertLevel)));
 	          		sendTiltNotification(tmpObj.id);
 	          		sendStolenNotification(tmpObj.id);
 	          	} else {
